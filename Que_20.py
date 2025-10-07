43e7f9fad28b43220f8ee6f658d3a1eb8466a62a
kuriya-shreyanshkumar <shreyanshkumar.kuriya@gmail.com>
2025-10-07 08:17:20 +0530

    Que_20.py

--- Que_20.py
+++ Que_20.py
@@ -1,38 +1,39 @@
-"""20.Mini project :
- Problem Statement : Password Generator
- Make a program to generate a strong password using the input given by the user.
- To generate a password, randomly take some words from the user input and then
- include numbers, special characters and capital letters to generate the password.
- Also, keep a check that password length is more than 8 characters.
- Note: Include Exception handling wherever required. Also, make a ‘User’ class
- and store the details like user id, name and password of each user as a tuple.
-"""
+class User:
+    def __init__(self, user_id, name, password):
+        if not password.strip():
+            raise ValueError("Password cannot be empty!")
+
+        self.user_id = user_id
+        self.name = name
+        self.password = self._generate_password(password)
+
+ 
+    def _generate_password(self, raw_password):
+        word = raw_password.split()[0]   
+        password = word.capitalize() + "123" + "@#"
+
+        if len(password) <= 8:
+            password += "XY"
+
+     
+        binary_pass = ' '.join(format(ord(char), '08b') for char in password)
+        return binary_pass
+
+    def display_details(self):
+        print("\nUser created successfully!\n")
+        print("User ID :", self.user_id)
+        print("Name    :", self.name)
+        print("Binary Password:", self.password)
+
+
+
 user_id = input("Enter User ID: ")
 name = input("Enter Name: ")
-user_input = input("Enter your password: ")
+password = input("Enter your password: ")
 
+try:
+    user = User(user_id, name, password)   
+    user.display_details()
+except ValueError as e:
+    print("Error:", e)
 
-if not user_input.strip():
-    raise ValueError("Input cannot be empty!")
-
-
-word = user_input.split()[0]
-
-
-password = word.capitalize() + "123" + "@#"
-
-
-if len(password) <= 8:
-    password = password + "XY"
-
-
-binary_pass = ' '.join(format(ord(char), '08b') for char in password)
-
-
-user_details = (user_id, name, binary_pass)
-
-
-print("\nUser created successfully!\n")
-print("User ID :", user_details[0])
-print("Name    :", user_details[1])
-print("Binary Password:", user_details[2])
