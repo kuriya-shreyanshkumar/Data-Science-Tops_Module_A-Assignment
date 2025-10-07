6a2dfc5f9c306f14855608156ff8b2e6b50b7f0f
kuriya-shreyanshkumar <shreyanshkumar.kuriya@gmail.com>
2025-10-07 08:15:05 +0530

    Que_19.py

--- Que_19.py
+++ Que_19.py
@@ -4,3 +4,9 @@
 def unique_elements(input_list):
     unique_list = list(set(input_list))
     return unique_list
+
+my_list = [1, 2, 2, 3, 4, 4, 5, 1]
+result = unique_elements(my_list)
+
+print("Original List:", my_list)
+print("Unique Elements:", result)
